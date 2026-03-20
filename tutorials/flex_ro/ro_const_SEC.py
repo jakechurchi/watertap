f2cf238b2fb9ec4f7bb24b3909b283d71285b570
Jake Churchill <jake.churchill@nrel.gov>
2026-03-20 17:14:42 -0600

    Filtering out printing shutdown events

--- tutorials/flex_ro/ro_const_SEC.py
+++ tutorials/flex_ro/ro_const_SEC.py
@@ -496,17 +496,23 @@ if __name__ == "__main__":
     # Can't use gurobi because it requires a liciense for integer variables
     # So going to use ipopt, but may need to look into this further
     # # dt = DiagnosticsToolbox(m)
-    # solver = get_solver()
-    # results = solver.solve(m)
+    solver = get_solver()
+    results = solver.solve(m)
 
-    mip_gap = 0.03
-    solver = pyo.SolverFactory("gurobi_direct_minlp")
-    solver.options["MIPGap"] = mip_gap
-    results = solver.solve(m, tee=True)
+    # mip_gap = 0.03
+    # solver = pyo.SolverFactory("gurobi_direct_minlp")
+    # solver.options["MIPGap"] = mip_gap
+    # results = solver.solve(m, tee=True)
 
     pyo.assert_optimal_termination(results)
 
-    print(m.get_design_var_values())
+    design_var_values = m.get_design_var_values()
+    filtered_design_var_values = {
+        k: v
+        for k, v in design_var_values.items()
+        if "num_flow_changes" not in k and "flow_changed" not in k
+    }
+    print(filtered_design_var_values)
 
     plot_function(m, n_time_points=len(price_data))
 
