a4136162caf2232fc7bbca6bad794499739728ca
Jake Churchill <jake.churchill@nrel.gov>
2026-04-01 17:13:47 -0600

    update energy equation to quadratic fit

--- tutorials/flex_ro/ro_const_RR_wrd.py
+++ tutorials/flex_ro/ro_const_RR_wrd.py
@@ -339,9 +339,9 @@ if __name__ == "__main__":
             "nominal_flowrate": 602,
             "maximum_flowrate": 635,
             "surrogate_type": "quadratic_energy_intensity",
-            "surrogate_a": 0.213771,
-            "surrogate_b": 0.000369,
-            "surrogate_c": 0,
+            "surrogate_a": 7.83e-1,
+            "surrogate_b": 1.588e-3,
+            "surrogate_c": 1.673e-6,
             "nominal_recovery": 0.92,
             "num_ro_skids": 4,
         }
