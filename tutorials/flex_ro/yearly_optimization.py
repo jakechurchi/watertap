02d610a09c5bdc7dc896780a550cb20042138acb
Jake Churchill <jake.churchill@nrel.gov>
2026-04-24 17:15:01 -0600

    Adding a file for the yearly optimization

---
+++ tutorials/flex_ro/yearly_optimization.py
@@ -0,0 +1,19 @@
+# imports
+
+# load surrogate data for summer and winter costs as function of water production (and # of rainy days)
+
+# create surrogate model for both
+
+# Create model
+
+# Define the variables (water production in each week)
+
+# Add surrogate models as constraints
+
+# Add constraints
+# total water production
+# Maximum water deficit?
+
+# Define the objective (minimize total cost)
+
+# Solve model w/ ipopt (should work?)
