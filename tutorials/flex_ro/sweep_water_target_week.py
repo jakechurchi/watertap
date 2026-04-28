b010dc03cd6481a94f75ab83d8c80d748a413234
Jake Churchill <rchurchi@x3000c0s33b0n0.head.cm.kestrel.hpc.nrel.gov>
2026-04-27 18:19:50 -0600

    lower mip gap solutions

--- tutorials/flex_ro/sweep_water_target_week.py
+++ tutorials/flex_ro/sweep_water_target_week.py
@@ -468,10 +468,10 @@ def one_week(annual_production_AF=13000):
     # solver.options["max_iter"] = 500
     # results = solver.solve(m, tee=True)
 
-    mip_gap = 0.0185
+    mip_gap = 0.01
     solver = pyo.SolverFactory("gurobi_direct_minlp")
     solver.options["MIPGap"] = mip_gap
-    solver.options["MIPFocus"] = 2
+    # solver.options["MIPFocus"] = 2
     # solver.options["StartNodeLimit"] = (
     #     50000  # I think this will allow it to complete the partial solution I'm initializing above.
     # )
