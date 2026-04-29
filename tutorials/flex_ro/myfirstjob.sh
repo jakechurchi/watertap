02909c18d4087352f4f75c2d513d4a4bda95c2e0
Jake Churchill <jake.churchill@nrel.gov>
2026-04-28 18:38:41 -0600

    removing the licience request

--- tutorials/flex_ro/myfirstjob.sh
+++ tutorials/flex_ro/myfirstjob.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+#SBATCH --job-name=pricetaker_test
 #SBATCH --account=nawianalysis
 #SBATCH --time=00:05:00
-#SBATCH --job-name=pricetaker_test
+#SBATCH --nodes=1
+#SBATCH --partition=short
 #SBATCH --mail-user=jake.churchill@nlr.gov
 #SBATCH --mail-type=ALL
 #SBATCH --output=job_output_filename.%j.out  # %j will be replaced with the job ID
-#SBATCH --L gurobi: 1
+
 
 module load gurobi
 module load anaconda3
