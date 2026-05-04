15f169b34716bd76f5f111f2cf1cbd3af4d073f0
Jake Churchill <jake.churchill@nrel.gov>
2026-05-04 16:06:48 -0600

    Updating batch job file

--- tutorials/flex_ro/const_RR_week.sh
+++ tutorials/flex_ro/const_RR_week.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=pricetaker_test
 #SBATCH --account=nawianalysis
-#SBATCH --time=00:20:00
-#SBATCH --nodes=1
+#SBATCH --time=00:45:00
+#SBATCH --nodes=2
 #SBATCH --partition=short
 #SBATCH -L gurobi:1
 #SBATCH --mail-user=jake.churchill@nlr.gov
