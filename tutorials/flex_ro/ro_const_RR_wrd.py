c71914bab037abb2f7d4b670801a2357300140ba
Jake Churchill <jake.churchill@nrel.gov>
2026-05-05 09:43:21 -0600

    increase mip gap

--- tutorials/flex_ro/ro_const_RR_wrd.py
+++ tutorials/flex_ro/ro_const_RR_wrd.py
@@ -504,7 +504,7 @@ if __name__ == "__main__":
     # solver.options["max_iter"] = 500
     # results = solver.solve(m, tee=True)
 
-    mip_gap = 0.01
+    mip_gap = 0.02
     solver = pyo.SolverFactory("gurobi_direct_minlp")
     solver.options["MIPGap"] = mip_gap
     # solver.options["MIPFocus"] = 1
