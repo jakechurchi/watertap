ba49d563a3f0e6156f947fbbef8d70def6946bdc
Jake Churchill <rchurchi@kl1.head.cm.kestrel.hpc.nlr.gov>
2026-05-13 23:13:36 -0600

    first result with new surrogate

--- tutorials/flex_ro/current_job.sh
+++ tutorials/flex_ro/current_job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=PT_with_DR
 #SBATCH --account=nawianalysis
-#SBATCH --time=00:30:00
+#SBATCH --time=00:10:00
 #SBATCH --nodes=2
 #SBATCH --partition=short
 #SBATCH -L gurobi:1
