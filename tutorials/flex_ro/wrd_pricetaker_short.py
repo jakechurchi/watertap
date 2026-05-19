6a45e89d3a464df37cfa4cc004462f9ffc45b95e
Jake Churchill <jake.churchill@nrel.gov>
2026-05-19 09:10:16 -0600

    Trying full flexibility case

--- tutorials/flex_ro/wrd_pricetaker_short.py
+++ tutorials/flex_ro/wrd_pricetaker_short.py
@@ -441,7 +441,7 @@ def main(season, flex_type, num_flexible_trains=4):
     # solver.options["max_iter"] = 500
     # results = solver.solve(m, tee=True)
 
-    mip_gap = 0.003
+    mip_gap = 0.015
     solver = pyo.SolverFactory("gurobi_direct_minlp")
     solver.options["MIPGap"] = mip_gap  # 2.0 %
     # solver.options["MIPGapAbs"] = (
@@ -518,9 +518,9 @@ def main(season, flex_type, num_flexible_trains=4):
 
 
 if __name__ == "__main__":
-    seasons = ["winter"]
-    flex_types = ["no_flex"]
-    num_flex_skids = [2]
+    seasons = ["winter", "summer"]
+    flex_types = ["both"]
+    num_flex_skids = [4]
 
     results_rows = []
 
