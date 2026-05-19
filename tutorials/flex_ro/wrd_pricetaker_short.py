3f229eb0b2e557b6f42a8add1e2d8cea18930d70
Jake Churchill <rchurchi@kl1.head.cm.kestrel.hpc.nlr.gov>
2026-05-19 08:55:36 -0600

    winter flex result

--- tutorials/flex_ro/wrd_pricetaker_short.py
+++ tutorials/flex_ro/wrd_pricetaker_short.py
@@ -441,7 +441,7 @@ def main(season, flex_type, num_flexible_trains=4):
     # solver.options["max_iter"] = 500
     # results = solver.solve(m, tee=True)
 
-    mip_gap = 0.007
+    mip_gap = 0.003
     solver = pyo.SolverFactory("gurobi_direct_minlp")
     solver.options["MIPGap"] = mip_gap  # 2.0 %
     # solver.options["MIPGapAbs"] = (
