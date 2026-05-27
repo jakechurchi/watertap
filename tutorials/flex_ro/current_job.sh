29bbc331c3229058da967c6a59dc6fc2dc819d01
Jake Churchill <rchurchi@kl2.head.cm.kestrel.hpc.nlr.gov>
2026-05-27 13:12:44 -0600

    MIQCP summer week result that timed out

--- tutorials/flex_ro/current_job.sh
+++ tutorials/flex_ro/current_job.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH --job-name=PT_week
 #SBATCH --account=nawianalysis
-#SBATCH --time=01:00:00
+#SBATCH --time=00:30:00
 #SBATCH --nodes=2
 #SBATCH --partition=short
 #SBATCH -L gurobi:1
 #SBATCH --mail-user=jake.churchill@nlr.gov
 #SBATCH --mail-type=ALL
-#SBATCH --output=PT_summer_week_linear_surr.%j.out  # %j will be replaced with the job ID
+#SBATCH --output=PT_MIQCP_summer_week.%j.out  # %j will be replaced with the job ID
 
 module load gurobi
 module load anaconda3
