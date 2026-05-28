9ab3a00784efd84b42bd20601e91c1a517acd600
Jake Churchill <rchurchi@kl1.head.cm.kestrel.hpc.nlr.gov>
2026-05-28 10:23:11 -0600

    Results for different num of flex trains

--- tutorials/flex_ro/long_job.sh
+++ tutorials/flex_ro/long_job.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH --job-name=PT_LONG_TEST
 #SBATCH --account=nawianalysis
-#SBATCH --time=48:00:00
+#SBATCH --time=12:00:00
 #SBATCH --nodes=2
 #SBATCH --partition=standard
 #SBATCH -L gurobi:1
 #SBATCH --mail-user=jake.churchill@nlr.gov
 #SBATCH --mail-type=ALL
-#SBATCH --output=PT_LONG_MIQCP.%j.out  # %j will be replaced with the job ID
+#SBATCH --output=PT_diff_flex_train_num.%j.out  # %j will be replaced with the job ID
 
 module load gurobi
 module load anaconda3
