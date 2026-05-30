b0066cb560aea0f11715f7214d3d763df9f02442
Jake Churchill <jake.churchill@nrel.gov>
2026-05-30 16:51:58 -0600

    Running job again with more time

--- tutorials/flex_ro/long_job.sh
+++ tutorials/flex_ro/long_job.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH --job-name=PT
 #SBATCH --account=nawianalysis
-#SBATCH --time=12:00:00
+#SBATCH --time=24:00:00
 #SBATCH --nodes=2
 #SBATCH --partition=standard
 #SBATCH -L gurobi:1
 #SBATCH --mail-user=jake.churchill@nlr.gov
 #SBATCH --mail-type=ALL
-#SBATCH --output=PT_2_flex_trains.%j.out  # %j will be replaced with the job ID
+#SBATCH --output=PT_flex_trains.%j.out  # %j will be replaced with the job ID
 
 module load gurobi
 module load anaconda3
