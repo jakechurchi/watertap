dae31f0228827e35721d30e3cc5ea87b0058c01e
Jake Churchill <rchurchi@kl2.head.cm.kestrel.hpc.nlr.gov>
2026-06-03 14:30:59 -0600

    result for 2 trains and TOU 8

--- tutorials/flex_ro/pricetaker_fast_5_28.py
+++ tutorials/flex_ro/pricetaker_fast_5_28.py
@@ -613,7 +613,7 @@ def main(season, flex_type, num_flexible_trains=4):
     # IPOPT
     # solver = get_solver()
 
-    mip_gap = 0.01
+    mip_gap = 0.014
     solver = pyo.SolverFactory("gurobi_direct_minlp")
     solver.options["MIPGap"] = mip_gap  # 1.0 %
     # solver.options["MIPGapAbs"] = (
@@ -676,7 +676,7 @@ def main(season, flex_type, num_flexible_trains=4):
 
 
 if __name__ == "__main__":
-    seasons = ["winter", "summer"]
+    seasons = ["summer"]
     flex_types = ["both"]
     num_flex_skids = [2]
 
