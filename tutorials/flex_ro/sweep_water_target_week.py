4412d655bc61629f887094b6da6abe0710bbad95
Jake Churchill <jake.churchill@nrel.gov>
2026-06-17 18:05:40 -0600

    Last water targ point full flex

--- tutorials/flex_ro/sweep_water_target_week.py
+++ tutorials/flex_ro/sweep_water_target_week.py
@@ -602,9 +602,9 @@ def one_week(annual_production_AF=13000, flex_type=None, season="summer"):
 
 if __name__ == "__main__":
     # Inputs
-    water_prod_targs = [9000, 10000, 11000, 13000, 15000]
+    water_prod_targs = [15000]
     season = "summer"
-    flex_type = "one_shutdown"
+    flex_type = "both"
 
     # Outputs
     water = []
