ddbb56063e313e6317251e6142398b57b206079b
Jake Churchill <jake.churchill@nrel.gov>
2026-06-29 17:58:25 -0600

    Trying with variable flowrate

--- tutorials/flex_ro/pricetaker_fast_5_28.py
+++ tutorials/flex_ro/pricetaker_fast_5_28.py
@@ -617,7 +617,7 @@ def main(season, flex_type, num_flexible_trains=4):
     # IPOPT
     # solver = get_solver()
 
-    mip_gap = 0.005
+    mip_gap = 0.001
     solver = pyo.SolverFactory("gurobi_direct_minlp")
     solver.options["MIPGap"] = mip_gap  # 1.0 %
     # solver.options["MIPGapAbs"] = (
@@ -681,7 +681,7 @@ def main(season, flex_type, num_flexible_trains=4):
 
 if __name__ == "__main__":
     seasons = ["winter"]
-    flex_types = ["no_flex"]
+    flex_types = ["both"]
     num_flex_skids = [2]
 
     results_rows = []
