a583eb38333602b36b96b946fc3ea879a0b8f0c1
Jake Churchill <rchurchi@kl1.head.cm.kestrel.hpc.nlr.gov>
2026-06-29 11:23:49 -0600

    smaller mip gap results for modular shutdowns

--- tutorials/flex_ro/current_job.sh
+++ tutorials/flex_ro/current_job.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name=PT_sweep
+#SBATCH --job-name=PT
 #SBATCH --account=nawianalysis
-#SBATCH --time=02:30:00
+#SBATCH --time=03:00:00
 #SBATCH --nodes=2
 #SBATCH --partition=short
 #SBATCH -L gurobi:1
 #SBATCH --mail-user=jake.churchill@nlr.gov
 #SBATCH --mail-type=ALL
-#SBATCH --output=water_sweep_num_shutdowns.%j.out  # %j will be replaced with the job ID
+#SBATCH --output=PT_mod_shutdowns.%j.out  # %j will be replaced with the job ID
 
 module load gurobi
 module load anaconda3
 conda activate watertap-pricetaker
-python sweep_water_target_week.py
+python pricetaker_fast_5_28.py
