0079200fabe6c0108b7ce1e155d1e95e7e5c502c
Jake Churchill <rchurchi@kl2.head.cm.kestrel.hpc.nlr.gov>
2026-06-30 15:10:10 -0600

    result for diff peak demand charges

--- tutorials/flex_ro/current_job.sh
+++ tutorials/flex_ro/current_job.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=PT
 #SBATCH --account=nawianalysis
-#SBATCH --time=03:00:00
+#SBATCH --time=01:00:00
 #SBATCH --nodes=2
-#SBATCH --partition=short
+#SBATCH --partition=debug
 #SBATCH -L gurobi:1
 #SBATCH --mail-user=jake.churchill@nlr.gov
 #SBATCH --mail-type=ALL
