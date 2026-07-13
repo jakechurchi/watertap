7a01fcc5fbc1fa8397be50c360852b319fb1cda6
Jake Churchill <jake.churchill@nrel.gov>
2026-07-13 17:07:32 -0600

    suspicious this result is optimal, lowering mip gap

--- tutorials/flex_ro/pricetaker_fast_5_28.py
+++ tutorials/flex_ro/pricetaker_fast_5_28.py
@@ -617,7 +617,7 @@ def main(season, flex_type, num_flexible_trains=4):
     # IPOPT
     # solver = get_solver()
 
-    mip_gap = 0.02
+    mip_gap = 0.01
     solver = pyo.SolverFactory("gurobi_direct_minlp")
     solver.options["MIPGap"] = mip_gap  # 1.0 %
     # solver.options["MIPGapAbs"] = (
@@ -692,7 +692,7 @@ def main(season, flex_type, num_flexible_trains=4):
 if __name__ == "__main__":
     seasons = ["summer"]
     flex_types = ["no_flex"]
-    num_flex_skids = [4]
+    num_flex_skids = [2]
 
     results_rows = []
 
