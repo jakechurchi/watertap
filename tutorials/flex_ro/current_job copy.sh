43a0ce55e0e97fbee4948507af04254c9255062b
Jake Churchill <jake.churchill@nrel.gov>
2026-07-17 10:49:39 -0600

    Creating copy of pricetaker script to test the 2026 rate structure

---
+++ tutorials/flex_ro/current_job copy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=PT
+#SBATCH --account=nawianalysis
+#SBATCH --time=24:00:00
+#SBATCH --nodes=2
+#SBATCH --partition=standard
+#SBATCH -L gurobi@slurmdb:1
+#SBATCH --mail-user=jake.churchill@nlr.gov
+#SBATCH --mail-type=ALL
+#SBATCH --output=PT_2026_week.%j.out  # %j will be replaced with the job ID
+
+module load gurobi
+module load anaconda3
+conda activate watertap-pricetaker
+python pricetaker_fast_5_28_copy.py
