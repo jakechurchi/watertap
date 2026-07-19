f10dbe9ea8fea2456a28bc51eb638dc0ec3ff019
Jake Churchill <rchurchi@kl2.head.cm.kestrel.hpc.nlr.gov>
2026-07-19 13:38:53 -0600

    results 2026 baseline

--- tutorials/flex_ro/current_job_copy.sh
+++ tutorials/flex_ro/current_job_copy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=PT
 #SBATCH --account=nawianalysis
-#SBATCH --time=24:00:00
+#SBATCH --time=00:10:00
 #SBATCH --nodes=2
-#SBATCH --partition=standard
+#SBATCH --partition=debug
 #SBATCH -L gurobi@slurmdb:1
 #SBATCH --mail-user=jake.churchill@nlr.gov
 #SBATCH --mail-type=ALL
