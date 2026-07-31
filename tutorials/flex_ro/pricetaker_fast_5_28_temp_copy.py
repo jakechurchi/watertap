0affea614debfda5dca504ad33b7f14db387b2cf
Jake Churchill <jake.churchill@nrel.gov>
2026-07-30 19:25:41 -0600

    testing winter week again

--- tutorials/flex_ro/pricetaker_fast_5_28_temp_copy.py
+++ tutorials/flex_ro/pricetaker_fast_5_28_temp_copy.py
@@ -378,8 +378,8 @@ def _begin_and_end_constraint(m):
 
 def main(season, flex_type, num_flexible_trains=4):
     season_map = {
-        "summer": "price_signals/wrd_pricesignal_summer_week_higher_rate_even.csv",
-        "winter": "price_signals/wrd_pricesignal_winter_week_2026.csv",
+        "summer": "price_signals/wrd_pricesignal_summer_week.csv",
+        "winter": "price_signals/wrd_pricesignal_winter_week.csv",
     }
     season_key = season.lower()
     if season_key not in season_map:
@@ -652,7 +652,7 @@ def main(season, flex_type, num_flexible_trains=4):
     # IPOPT
     # solver = get_solver()
 
-    mip_gap = 0.0075
+    mip_gap = 0.005
     solver = pyo.SolverFactory("gurobi_direct_minlp")
     solver.options["MIPGap"] = mip_gap  # 1.0 %
     # solver.options["MIPGapAbs"] = (
@@ -725,7 +725,7 @@ def main(season, flex_type, num_flexible_trains=4):
 
 
 if __name__ == "__main__":
-    seasons = ["summer"]
+    seasons = ["winter"]
     flex_types = ["both"]
     num_flex_skids = [4]
 
