710ec51fd29a86b145e74d4d110feab78d19fcf6
Jake Churchill <rchurchi@kl1.head.cm.kestrel.hpc.nlr.gov>
2026-08-18 12:55:16 -0600

    test result with working hours

--- tutorials/flex_ro/pricetaker_fast_5_28.py
+++ tutorials/flex_ro/pricetaker_fast_5_28.py
@@ -667,7 +667,7 @@ def main(season, flex_type, num_flexible_trains=4):
     # IPOPT
     # solver = get_solver()
 
-    mip_gap = 0.025
+    mip_gap = 0.015
     solver = pyo.SolverFactory("gurobi_direct_minlp")
     solver.options["MIPGap"] = mip_gap  # 1.0 %
     # solver.options["MIPGapAbs"] = (
