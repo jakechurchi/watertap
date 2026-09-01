14e1682224038fa4755cd895630ae0c2668a12a4
Jake Churchill <jake.churchill@nrel.gov>
2026-09-01 09:13:59 -0600

    no start-up cost RTP

--- tutorials/flex_ro/PT_wrd_week_temp_copy.py
+++ tutorials/flex_ro/PT_wrd_week_temp_copy.py
@@ -574,7 +574,7 @@ def main(season, flex_type, num_flexible_trains=4):
     )
 
     brine_cost_start_up_mod = (
-        -0.5
+        -1
     )  # If this is 1, the brine cost is doubled during startup. If it is 2, it is tripled. If it is -1, there is no brine cost during startup.
 
     fs.add_flow_costs(
