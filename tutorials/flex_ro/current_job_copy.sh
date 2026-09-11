e89d6c23f3cb4bfc1c1d339e01c6da0e9a372b26
Jake Churchill <jake.churchill@nrel.gov>
2026-09-11 15:34:15 -0600

    TOU8 baseline and RTP README

--- tutorials/flex_ro/current_job_copy.sh
+++ tutorials/flex_ro/current_job_copy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH --job-name=PT_copy
 #SBATCH --account=nawianalysis
-#SBATCH --time=00:30:00
+#SBATCH --time=00:10:00
 #SBATCH --nodes=2
 #SBATCH --partition=debug
 #SBATCH -L gurobi@slurmdb:1
 #SBATCH --mail-user=jake.churchill@nlr.gov
 #SBATCH --mail-type=ALL
-#SBATCH --output=PT_RTP_baseline.%j.out  # %j will be replaced with the job ID
+#SBATCH --output=PT_TOU8_baseline.%j.out  # %j will be replaced with the job ID
 
 module load gurobi
 module load anaconda3
---
+++ tutorials/flex_ro/operations plots/Rate Structures/RTP/_README.md
@@ -0,0 +1,9 @@
+-----
+NOTE: summer_baseline_RTP and summer_full_flex_RTP are results with the updated and correct rate file.
+-----
+
+NAMING CONVENTION:
+no_mod_trains --> three RO trains remain in operation at all times but the recovery and flowrate can change.
+
+METHODS NOTE:
+- The RTP rate is from 2025, but divided by 1.06 to account for inflation between 2025 and 2023. The exact rates were shared with me by Pooria.
