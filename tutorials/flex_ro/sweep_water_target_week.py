89e85d59ca41a8414abc7c6649559e85cde66820
Jake Churchill <rchurchi@kl2.head.cm.kestrel.hpc.nlr.gov>
2026-09-22 10:30:29 -0600

    results for 9000AF no shutdowns

--- tutorials/flex_ro/sweep_water_target_week.py
+++ tutorials/flex_ro/sweep_water_target_week.py
@@ -593,7 +593,7 @@ def one_week(
     # solver = get_solver()
     # solver.options["max_iter"] = 500
 
-    mip_gap = 0.01
+    mip_gap = 0.0139
     solver = pyo.SolverFactory("gurobi_direct_minlp")
     solver.options["MIPGap"] = mip_gap  # 1.0 %
     # solver.options["MIPGapAbs"] = (
